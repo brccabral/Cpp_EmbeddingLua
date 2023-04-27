4d85f52e7c77346e62560c4f0c93a41d47836fde
Bruno Cabral <bruno.cabral@amdocs.com>
2023-04-27 15:43:19 -0700

    lua persistence

--- embed_lua.cpp
+++ embed_lua.cpp
@@ -46,6 +46,18 @@ int main()
         }
     }
 
+    cmd = "a = a + 100";
+    
+    if (CheckLua(L, luaL_dostring(L, cmd.c_str())))
+    {
+        lua_getglobal(L, "a");
+        if (lua_isnumber(L, -1))
+        {
+            float a_in_cpp = (float)lua_tonumber(L, -1);
+            std::cout << "a_in_cpp = " << a_in_cpp << std::endl;
+        }
+    }
+
     system("pause");
     lua_close(L);
 
